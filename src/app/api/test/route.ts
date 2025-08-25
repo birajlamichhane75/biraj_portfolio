aec23d7255f1ffc153a587cc8e66f0bb1e15eef0
Biraj Lamichhane <birajlamichhane57@gmail.com>
2025-08-25 01:45:40 -0500

    error fixed in api/test

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,18 +1,19 @@
-import { NextResponse } from "next/server";
-import { adminDb } from "../../../lib/firebaseAdmin";
+// import { NextResponse } from "next/server";
+// import { adminDb } from "../../../lib/firebaseAdmin";
 
-export async function GET() {
-  try {
-    const docRef = await adminDb.collection("testCollection").add({
-      message: "Hello from Admin SDK 🚀",
-      createdAt: new Date().toISOString(),
-    });
+// export async function GET() {
+//   try {
+//     const docRef = await adminDb.collection("testCollection").add({
+//       message: "Hello from Admin SDK 🚀",
+//       createdAt: new Date().toISOString(),
+//     });
 
-    const snapshot = await adminDb.collection("testCollection").get();
-    const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+//     const snapshot = await adminDb.collection("testCollection").get();
+//     const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 
-    return NextResponse.json({ success: true, addedId: docRef.id, allDocs: data });
-  } catch (error: any) {
-    return NextResponse.json({ success: false, error: error.message }, { status: 500 });
-  }
-}
+//     return NextResponse.json({ success: true, addedId: docRef.id, allDocs: data });
+//   } catch (error: any) {
+//     return NextResponse.json({ success: false, error: error.message }, { status: 500 });
+//   }
+// }
+// // 
